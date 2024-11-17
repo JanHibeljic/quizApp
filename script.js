69044c1b17b819813657ce836c24a967b065d6f7
Jan Hibeljic <j.hibeljic@gmx.de>
2024-11-18 00:48:39 +0100

    finished project without cleancoding

--- script.js
+++ script.js
@@ -57,3 +57,97 @@ let questions = [
     right_answer: 4,
   },
 ];
+let currentQuestion = 0;
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio("audio/success.mp3");
+let AUDIO_FAIL = new Audio("audio/fail.mp3");
+
+function init() {
+  document.getElementById("allQuestions").innerHTML = questions.length;
+  showQuestion();
+}
+
+function showQuestion() {
+  if (currentQuestion >= questions.length) {
+    //Show End Screen
+    document.getElementById("endScreen").style = "";
+    document.getElementById("questionBody").style = "display: none";
+    document.getElementById("CorrectAnswerTotal").innerHTML = questions.length;
+    document.getElementById("CorrectAnswer").innerHTML = rightQuestions;
+    document.getElementById("headerImage").src =
+      "./images/carnival-631151_1280.jpg";
+  } else {
+    //Show Question
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById("progressBar").innerHTML = `${percent}%`;
+    document.getElementById("progressBar").style = `width:${percent}%`;
+    console.log("fortschritt:", percent);
+    let question = questions[currentQuestion];
+    document.getElementById("currentQuestionSite").innerHTML =
+      currentQuestion + 1;
+
+    document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
+}
+
+function answer(selection) {
+  let question = questions[currentQuestion];
+  //   console.log(" ausgesucht", selection);
+  let selectedQuestionNumber = selection.slice(-1);
+  //   console.log(" selected question Number:", selectedQuestionNumber);
+  //   console.log("question is:", question["right_answer"]);
+  let idOfRightAnswer = `answer_${question["right_answer"]}`;
+
+  if (selectedQuestionNumber == question["right_answer"]) {
+    console.log("richtige Antwort");
+    document.getElementById(selection).parentNode.classList.add("bg-success");
+    AUDIO_SUCCESS.play();
+    rightQuestions++;
+  } else {
+    console.log("falsche Antwort");
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+    AUDIO_FAIL.play();
+  }
+  document.getElementById("nextButton").disabled = false;
+}
+
+function nextQuestion() {
+  currentQuestion++; //z.B. von Frage 0 auf 1
+  showQuestion();
+  document.getElementById("nextButton").disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+}
+
+function resetAnswerButtons() {
+  document
+    .getElementById("answer_1")
+    .parentNode.classList.remove("bg-success", "bg-danger");
+  document
+    .getElementById("answer_2")
+    .parentNode.classList.remove("bg-success", "bg-danger");
+  document
+    .getElementById("answer_3")
+    .parentNode.classList.remove("bg-success", "bg-danger");
+  document
+    .getElementById("answer_4")
+    .parentNode.classList.remove("bg-success", "bg-danger");
+}
+
+function restartGame() {
+  document.getElementById("headerImage").src =
+    "images/quiz-time-9163843_1280.jpg";
+  document.getElementById("endScreen").style = "display: none";
+  document.getElementById("questionBody").style = "";
+  currentQuestion = 0;
+  rightQuestions = 0;
+  init();
+}
